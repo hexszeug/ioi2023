3b6e1590add8340cb87a524adef76a6c404a41c2
Siemen Zielke <77232580+hexszeug@users.noreply.github.com>
2022-10-31 14:28:03 +0100

    mac build task

---
+++ test.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    vector<string> test = {"Hello \n", "World!"};
+    for (int i = 0; i < test.size(); i++) {
+        cout << test[i];
+    }
+    cout << endl;
+    return 0;
+}
