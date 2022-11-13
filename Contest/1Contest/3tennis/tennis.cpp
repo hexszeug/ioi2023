304af67728e021c6ee30448f515475e035a0cfc0
hexszeug <77232580+hexszeug@users.noreply.github.com>
2022-11-13 12:56:27 +0100

    100points 3tennis

--- Contest/1Contest/3tennis/tennis.cpp
+++ Contest/1Contest/3tennis/tennis.cpp
@@ -3,51 +3,41 @@
 using namespace std;
 
 int main() {
+    ios_base::sync_with_stdio(false);
     int N;
     cin >> N;
-    vector<pair<int, int>> eta(N), pwa(N), games;
+    vector<pair<int, int>> us(N), op(N);
     for (int i = 0; i < N; i++) {
         int a;
         cin >> a;
-        eta[i] = {a, i+1};
+        us[i] = {a, i+1};
     }
     for (int i = 0; i < N; i++) {
         int a;
         cin >> a;
-        pwa[i] = {a, i+1};
+        op[i] = {a, i+1};
     }
-    sort(eta.rbegin(), eta.rend());
-    sort(pwa.rbegin(), pwa.rend());
-    list<pair<int, int>> us, op;
-    for (int i = 0; i < N; i++) {
-        us.push_back(eta[i]);
-        op.push_back(pwa[i]);
-    }
-    vector<int> usb, opb;
-    for (auto opp : op) {
-        bool flag = true;
-        for (auto usp = us.begin(); usp != us.end();) {
-            auto last = usp;
+    sort(us.rbegin(), us.rend());
+    sort(op.rbegin(), op.rend());
+    stack<int> usb;
+    stack<pair<int, int>> games;
+    int usp = 0, opp = 0;
+    for (; opp < N; opp++, usp++) {
+        while (usp < N && us[usp].first > op[opp].first) {
+            usb.push(us[usp].second);
             usp++;
-            if (last->first < opp.first) {
-                games.emplace_back(last->second, opp.second);
-                us.erase(last);
-                flag = false;
-                break;
-            }
-            usb.push_back(last->second);
-            us.erase(last);
         }
-        if (flag) {
-            opb.push_back(opp.second);
-        }
+        if (usp >= N) break;
+        games.push(make_pair(us[usp].second, op[opp].second));
     }
     cout << games.size() << endl;
-    for (auto game : games) {
-        cout << game.first << " " << game.second << endl;
+    while (!games.empty()) {
+        cout << games.top().first << " " << games.top().second << "\n";
+        games.pop();
     }
-    for (int i = 0; i < usb.size(); i++) {
-        cout << usb[i] << " " << opb[i] << endl;
+    for (; opp < N; opp++) {
+        cout << usb.top() << " " << op[opp].second << "\n";
+        usb.pop();
     }
     return 0;
 }
