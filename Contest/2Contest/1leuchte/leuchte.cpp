ca2fecd89d56ec4492c8149b72dd968f9582881b
Siemen Zielke <77232580+hexszeug@users.noreply.github.com>
2022-12-04 14:27:59 +0100

    contest end 50 points total

--- Contest/2Contest/1leuchte/leuchte.cpp
+++ Contest/2Contest/1leuchte/leuchte.cpp
@@ -2,47 +2,49 @@
 
 using namespace std;
 
-int popFirstAvailable(list<int> &a, list<int> &b, list<int> &c) {
-    if (!a.empty()) {int res = a.back(); a.pop_back(); return res;}
-    if (!b.empty()) {int res = b.back(); b.pop_back(); return res;}
-    int res = c.back(); c.pop_back(); return res;
-}
-
 int main() {
     short n;
     cin >> n;
-    vector<signed char> sockets(n, 0), bulbs(n, 0); //0 = unknown, 1 = does not work, 2 = works
+    vector<signed char> sockets(n, 0), bulbs(n, 0); //0 = unknown, 1 = broken, 2 = works
     vector<vector<bool>> worksNotWithMatrix(n, vector<bool>(n, false));
+    vector<short> question(n, -1);
+    list<short> unknownBulbs, brokenBulbs, workingBulbs;
     bool unknownExist = true;
     while (unknownExist) {
         //ask
-        vector<short> question(n, -1);
-        list<short> unknownBulbs, notWorkingBulbs, workingBulbs;
+        question.clear();
+        question.resize(n);
+        unknownBulbs.clear();
+        brokenBulbs.clear();
+        workingBulbs.clear();
+        //prepare lists
         for (int i = 0; i < n; i++) {
             switch (bulbs[i]) {
                 case 0: unknownBulbs.push_back(i); break;
-                case 1: notWorkingBulbs.push_back(i); break;
+                case 1: brokenBulbs.push_back(i); break;
                 case 2: workingBulbs.push_back(i); break;
             }
         }
+        //match working sockets with unknown bulbs
         for (int i = 0; i < n; i++) {
             if (unknownBulbs.empty()) break;
             if (sockets[i] != 2) continue;
             question[i] = unknownBulbs.front();
             unknownBulbs.pop_front();
         }
+        //match unknown sockets with working bulbs
         for (int i = 0; i < n; i++) {
             if (workingBulbs.empty()) break;
             if (sockets[i] != 0) continue;
             question[i] = workingBulbs.front();
             workingBulbs.pop_front();
         }
+        //match rest without asking the same thing twice
         workingBulbs.splice(workingBulbs.end(), unknownBulbs);
-        workingBulbs.splice(workingBulbs.end(), notWorkingBulbs);
+        workingBulbs.splice(workingBulbs.end(), brokenBulbs);
         for (int i = 0; i < n; i++) {
             if (question[i] != -1) continue;
             auto bulb = workingBulbs.begin();
-            bool miss = true;
             while (bulb != workingBulbs.end()) {
                 if (worksNotWithMatrix[i][*bulb]) {
                     bulb++;
@@ -50,21 +52,17 @@ int main() {
                 }
                 question[i] = *bulb;
                 bulb = workingBulbs.erase(bulb);
-                miss = false;
                 break;
-            }
-            if (miss) {
-                question[i] = -2;
             }
         }
         for (int i = 0; i < n; i++) {
-            if (question[i] == -2) {
+            if (question[i] == -1) {
                 question[i] = workingBulbs.front();
                 workingBulbs.pop_front();
             }
         }
         cout << "Q";
-        for (auto i : question) cout << " " << i+1;
+        for (int i = 0; i < n; i++) cout << " " << question[i]+1;
         cout << endl;
         //read
         for (int i = 0; i < n; i++) {
@@ -117,22 +115,21 @@ int main() {
             }
         }
     }
-    stack<short> workingBulbs, otherBulbs;
     for (int i = 0; i < n; i++) {
-        if (bulbs[i] == 2) workingBulbs.push(i);
-        else otherBulbs.push(i);
+        if (bulbs[i] == 2) workingBulbs.push_back(i);
+        else brokenBulbs.push_back(i);
     }
     cout << "A";
     for (int i = 0; i < n; i++) {
         if (sockets[i] == 2) {
             if (!workingBulbs.empty()) {
-                cout << " " << workingBulbs.top()+1;
-                workingBulbs.pop();
+                cout << " " << workingBulbs.front()+1;
+                workingBulbs.pop_front();
                 continue;
             }
         }
-        cout << " " << otherBulbs.top()+1;
-        otherBulbs.pop();
+        cout << " " << brokenBulbs.front()+1;
+        brokenBulbs.pop_front();
     }
     cout << endl;
     return 0;
--- Contest/2Contest/3bintree/bintree.cpp
+++ Contest/2Contest/3bintree/bintree.cpp
@@ -3,7 +3,12 @@
 using namespace std;
 
 int main() {
-    std::ios_base::sync_with_stdio(false);
-    
+    // std::ios_base::sync_with_stdio(false);
+    int i = 0;
+    int in = 1;
+    for (; in == 1; i++) {
+        scanf("%1d", &in);
+    }
+    cout << "0 " << (i + (i % 2)) / 2 << endl;
     return 0;
 }
